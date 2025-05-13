4e6cf08ec9f6bb25edd744f7e919156e713a43f6
BlinhNhi <trantoan1682016@gmail.com>
2025-05-13 17:02:44 +0700

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+
+    },
   },
   plugins: [],
 }
-
